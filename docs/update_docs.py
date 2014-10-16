055894dbb8f2a93321b17a7d939032b2dcbf1e41
Nick Roth <nlr06886@gmail.com>
2014-10-16 17:21:02 -0500

    Added docs to update_docs script.

--- docs/update_docs.py
+++ docs/update_docs.py
@@ -4,5 +4,8 @@ __author__ = 'Nick'
 import subprocess
 from shutil import copy
 
+# copy .gitignore to build directory, so git ignores .idea files
 copy('../.gitignore', './_build')
+
+# call ghp-import on build directory, which overwrites build to gh-pages branch
 subprocess.call('python C://Anaconda/Scripts/ghp-import -n ./_build')
