3f354d74ee80e10ae34f6ddf93c868dd84f1aa3a
Nick Roth <nlr06886@gmail.com>
2015-04-16 16:15:54 -0500

    Additional doc strings and reorganization.

--- formulapy/data/ergast.py
+++ formulapy/data/ergast.py
@@ -9,6 +9,7 @@ ergast_client = JSONClient(root_uri='http://ergast.com/api/')
 
 
 class ErgastSeason(Resource):
+    """Configures the Ergast API for retrieving Seasons."""
     class Meta:
         list = r'^f1/.json$'
         item = r'^f1/(?P<year>).json$'
@@ -20,7 +21,10 @@ def strip_header(payload):
 
 
 def get_races_from_season(season):
-    season = season['RaceTable']
+    """Strips off extra header data and just returns an array of races."""
+
+    mrdata = strip_header(season)
+    season = mrdata['RaceTable']
     races = season['Races']
     return races
 
@@ -29,7 +33,6 @@ def get_races(year):
     """Get list of races for the given year."""
 
     ssn = ErgastSeason.objects.get(year=year, client=ergast_client)
-    ssn = strip_header(ssn)
     json_races = get_races_from_season(ssn)
 
     races = []
